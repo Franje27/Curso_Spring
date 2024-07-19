01df496dfbb5e83495768067d663acb2e25cbbe6
Franje27 <franje27@gmail.com>
2024-07-19 13:36:31 +0200

    crear base de datos

--- 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/App.java
+++ 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/App.java
@@ -1,13 +1,29 @@
 package com.example.spring_web_mvc;
 
+import com.example.spring_web_mvc.controller.ProductController;
+import com.example.spring_web_mvc.entity.Product;
+import com.example.spring_web_mvc.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+         ApplicationContext context = SpringApplication.run(App.class, args);
+         var repository = context.getBean(ProductRepository.class);
+
+         List<Product> products = List.of(
+                 new Product(null, "product1", 5.99, 1),
+                 new Product(null, "product2", 6.99, 2),
+                 new Product(null, "product3", 7.99, 4),
+                 new Product(null, "product4", 8.99, 1)
+         );
+         repository.saveAll(products);
     }
 
 }
--- 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/controller/ProductController.java
+++ 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.example.spring_web_mvc.controller;
 
+import com.example.spring_web_mvc.entity.Product;
 import com.example.spring_web_mvc.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products ")
@@ -19,7 +22,8 @@ public class ProductController {
     @GetMapping
     public String findAll(Model model){
 
-        model.addAttribute("products", "Hola mundo");
+        List<Product> products = this.repository.findAll();
+        model.addAttribute("products", products);
         return "product-list";
 
     }
