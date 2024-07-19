40e717384d034b73c17171517d2eea63e8a50bda
Franje27 <franje27@gmail.com>
2024-07-19 14:47:18 +0200

    finalizar detalles finales

--- 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/controller/ProductController.java
+++ 002_Tu_primera_web_app_con_spring_boot/spring_web_mvc/src/main/java/com/example/spring_web_mvc/controller/ProductController.java
@@ -5,12 +5,15 @@ import com.example.spring_web_mvc.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+
 @Controller
-@RequestMapping("/products ")
+@RequestMapping("/products")
 public class ProductController {
 
     private ProductRepository repository;
@@ -28,6 +31,31 @@ public class ProductController {
 
     }
 
+     // GET http://localhost:8080/products/new
+    @GetMapping("/new")
+    public String getForm (Model model){
+
+        model.addAttribute("products", new Product());
+        return "product-form";
+
+    }
+
+    // POST http://localhost:8080/products
+    @PostMapping
+    public String save(@ModelAttribute("product") Product product){
+
+        this.repository.save(product);
+        return "redirect:/products";
+
+    }
+
+    // GET http://localhost:8080/products/{id}/view
+
+    // GET http://localhost:8080/products/{id}/edit
+
+    // GET http://localhost:8080/products/{id}/delete
+
+    // GET http://localhost:8080/products/delete/all
 
 
 }
